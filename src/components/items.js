98276ed5fd837a1aaedd5c94a8ee07a4bd693229
gokul299 <prasathgokul19@gmail.com>
2022-01-11 20:09:57 +0530

    first commit

---
+++ src/components/items.js
@@ -0,0 +1,41 @@
+let items = [
+    {
+        id: 1,
+        image: "./shop/fancy-products.jpg",
+        title: "Fancy Product",
+        cost: " $40.00 - $80.00",
+        btn: "View option",
+        text: false
+    },
+    {
+        id: 2,
+        image: "./shop/special.jpg",
+        title: "Special Items",
+        rating: "⭐⭐⭐⭐⭐",
+        cost: "$18.00",
+        btn: "Add to cart",
+        text: false
+    },
+    {
+        id: 3,
+        image: "./shop/sale.jpg",
+        title: "Sale Item",
+        cost: "$10.00 - $25.00",
+        btn: "Add to cart",
+        text: false
+    },
+    {
+        id: 4,
+        image: "./shop/popular.jpg",
+        title: "Popular Item",
+        rating: "⭐⭐⭐⭐⭐",
+        cost: " $40.00",
+        btn: "Add to cart",
+        text: false
+    },
+
+
+
+]
+
+export default items;
